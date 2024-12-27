d38846dd4ca809c5cca97391615b20f0095fa615
BHC1117 <162395649+BHC1117@users.noreply.github.com>
2024-12-27 14:41:10 +0900

    Title: 1까지 나누기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241227/1까지 나누기/divide-by-1.java
+++ 241227/1까지 나누기/divide-by-1.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc= new Scanner(System.in);
         int n= sc.nextInt();
-        int cnt=0;
+        int cnt=1;
         for(int i=1;i<=n;i++){
             cnt+=1;
             n/=i;
@@ -12,6 +12,6 @@ public class Main {
                 break;
             }
         }
-        System.out.print(cnt+1);
+        System.out.print(cnt);
     }
 }
