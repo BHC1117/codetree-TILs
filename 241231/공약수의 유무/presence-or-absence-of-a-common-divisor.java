3db85795aa6a0cd95f7fe64c9a5a5da6cb881546
BHC1117 <162395649+BHC1117@users.noreply.github.com>
2024-12-31 15:52:04 +0900

    Title: 공약수의 유무, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241231/공약수의 유무/presence-or-absence-of-a-common-divisor.java
+++ 241231/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -5,32 +5,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-
-        // 1920과 2880의 최대공약수 계산
-        int gcd = gcd(1920, 2880);
-
-        // 최대공약수의 모든 약수 확인
+        boolean satisfied = false;
+        
         boolean found = false;
-        for (int i = 1; i <= gcd; i++) {
-            if (gcd % i == 0) { // i가 최대공약수의 약수인지 확인
-                if (i >= a && i <= b) { // i가 범위 내에 있는지 확인
-                    found = true;
-                    break;
-                }
+        for (int i = a; i <=b; i++) {
+            if (1920%i==0 && 2880%i==0){
+                satisfied = true;
             }
+            
         }
-
-        // 결과 출력
-        System.out.println(found ? 1 : 0);
-    }
-
-    // 유클리드 호제법으로 최대공약수 계산
-    private static int gcd(int x, int y) {
-        while (y != 0) {
-            int temp = y;
-            y = x % y;
-            x = temp;
-        }
-        return x;
+        if(satisfied == true)
+        System.out.println(1);
+        else
+        System.out.println(0);
     }
 }
